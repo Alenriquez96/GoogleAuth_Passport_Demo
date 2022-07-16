562e7cf7e5bff3f73986e542ee6964f98a98d868
alenriquez96 <alberto.enriquez96@gmail.com>
2022-07-16 23:28:34 +0200

    finished

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
 
+//Establecemos la estrategia de Google con los credenciales de nuestro proyecto
 passport.use(new GoogleStrategy({
     clientID: `${process.env.CLIENT_ID}`,
     clientSecret: `${process.env.CLIENT_SECRET}`,
@@ -13,9 +14,11 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+//Esta función determina los datos que se van a guardar en la sesión de google: user
 passport.serializeUser(function (user, done) {
     done(null,user)
-})
+});
+//Determina que objeto borrar de la sesión: user
 passport.deserializeUser(function (user, done) {
     done(null,user)
 })
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const config = require("./config/config.js");
 const passport = require("passport");
 const session = require("express-session");
-require("./config/auth.js");
+require("./auth.js");
 
 const app = express();
 const port = 3000;
 
 app.use(session({ secret: 'SECRET'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req,res)=>{
     res.send('<a href="/auth/google">Authenticate with google </a>')
@@ -16,33 +17,44 @@ app.get("/", (req,res)=>{
 
 app.get("/auth/google", passport.authenticate("google", { scope: ['email', 'profile'], prompt: "select_account" }));
 
-app.get("/google/callBack", passport.authenticate('google', { failureRedirect: '/auth/failure' }), 
+//Esta ruta tiene dos funciones, la primera es en caso de fallo nos redirecciona a /auth/failure, y la segunda, en caso de éxito realiza la función siguiente.
+app.get("/google/callBack", 
+    //Función de fallo
+    passport.authenticate('google', { failureRedirect: '/auth/failure' }), 
+    //Función exitosa
     (req,res)=>{
+    //En el cuerpo de esta función podemos almacenar usuarios en nuestra bbdd con el objeto que nos proporciona req.user
+
+    //Estos son los pasos para crear un token si la autenticación es exitosa
     const payload = {
         //save here data
         check: true
     };
-    const token = jwt.sign(payload, config.key, {
+    const token = jwt.sign(payload, `secret_key`, {
         expiresIn: "20m"
     });
+
+    //Almacenamos el token en las cookies
     res.cookie("access-token", token, {
         httpOnly: true,
         sameSite: "strict",
-    })
-    res.send("Welcome! You are now authenticated with google! <a href='/logout'>Click here to logout!</a>");
+    }).send("Welcome! You are now authenticated with google! <br><br> <a href='/logout'>Click here to logout!</a>");
 });
 
-
+//Definimos una ruta en caso de que la autenticación falle.
 app.get('/auth/failure', (req, res) => {
     res.send('Something went wrong..')  
 });
 
+//Definimos la ruta de logout, donde eliminamos la sesión y limpiamos el token de las cookies.
 app.get('/logout', (req, res) => {
-    req.logout();
-    req.session.destroy();
-    res.send('Goodbye!');
-});
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        req.session.destroy();
+        res.clearCookie("access-token").send('Goodbye! <br><br> <a href="/auth/google">Authenticate again</a>');
+      });
 
+});
 
 
 app.listen(port, ()=>{
